1e9730ea1716c177f39c418011de5610608e0ebe
SHYAAM KRISHNAN R <168326697+Shyam-Krishnan24@users.noreply.github.com>
2026-02-07 15:18:48 +0530

    Add files via upload

---
+++ Source-Code/symptoms.py
@@ -0,0 +1,12 @@
+SYMPTOMS = {
+    "1": "Fever",
+    "2": "Headache",
+    "3": "Chest Pain",
+    "4": "Breathing Difficulty",
+    "5": "Abdominal Pain",
+    "6": "Vomiting / Nausea",
+    "7": "Dizziness / Fainting",
+    "8": "Injury / Bleeding",
+    "9": "Fatigue / Weakness",
+    "0": "Voice Description"
+}
